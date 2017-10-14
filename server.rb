a6d677f0977ac4c20c9c54653a1d988b9e1994cc
Christian Samuel <christian.leumas@icloud.com>
2017-10-13 22:51:10 -0400

    set url to post.

--- server.rb
+++ server.rb
@@ -2,9 +2,11 @@ require 'sinatra'
 require 'random_restaurant_selector'
 
 
-get '/' do
-puts request_hash = { location: ENV['MY_LOCATION'], term: 'food', radius: 900, open_now: false, price: '1,2', limit: 50 }
-puts search = RandomRestaurantSelector::Search.new(request_hash)
-puts restaurant = search.gimme_a_restaurant(search.get_businesses)
-puts restaurant.send_to_slack
+post '/' do
+  puts params
+  request_hash = { location: ENV['MY_LOCATION'], term: 'food', radius: 900, open_now: false, price: '1,2', limit: 50 }
+  search = RandomRestaurantSelector::Search.new(request_hash)
+  restaurant = search.gimme_a_restaurant(search.get_businesses)
+  restaurant.send_to_slack
+  puts params
 end
